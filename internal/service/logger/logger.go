e8980114e8db6b86a3c6bdcab1e7658f008529bb
basega <bassmky19940208@gmail.com>
2025-04-28 01:07:36 +0900

    fix

--- internal/service/logger/logger.go
+++ internal/service/logger/logger.go
@@ -9,12 +9,12 @@ import (
 type LoggerImpl struct{}
 
 // Error implements logger.Logger.
-func (l *LoggerImpl) Error(format string, args ...interface{}) {
+func (l *LoggerImpl) Info(format string, args ...interface{}) {
 	log.Printf("[INFO] "+format, args...)
 }
 
 // Info implements logger.Logger.
-func (l *LoggerImpl) Info(format string, args ...interface{}) {
+func (l *LoggerImpl) Error(format string, args ...interface{}) {
 	log.Printf("[ERROR] "+format, args...)
 }
 
